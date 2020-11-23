692c8653df64f78f0878fd4e764b93131e2f6088
Szx <18524359400@163.com>
2020-11-23 13:50:58 +0800

    11

---
+++ src/main/java/com/dragon/力扣/动态规划/OFFER剪绳子.java
@@ -0,0 +1,39 @@
+package com.dragon.力扣.动态规划;
+
+/**
+ * 状态转移方程：
+ * 当 i≥2 时，假设对正整数 i 拆分出的第一个正整数是 j（1≤j<i），则有以下两种方案：
+ *
+ * 将 i 拆分成 j 和 i−j 的和，且 i−j 不再拆分成多个正整数，此时的乘积是 j×(i−j)；
+ * 将 i 拆分成 j 和 i−j 的和，且 i−j 继续拆分成多个正整数，此时的乘积是 j×dp[i−j]。
+ *
+ */
+public class OFFER剪绳子 {
+    //动态规划时间复杂度高
+    public int cuttingRope(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i]= Math.max(dp[i], Math.max(j * (i - j), j * dp[i - j]));
+            }
+        }
+        return dp[n];
+    }
+
+    //利用数学方法（就以三为基准）
+    public int integerBreak(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+        int quotient = n / 3;
+        int remainder = n % 3;
+        if (remainder == 0) {
+            return (int) Math.pow(3, quotient);
+        } else if (remainder == 1) {
+            return (int) Math.pow(3, quotient - 1) * 4;
+        } else {
+            return (int) Math.pow(3, quotient) * 2;
+        }
+    }
+
+}
