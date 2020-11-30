b0ddfc7b058430b8174fa777bcd96394fa4b7333
Szx <18524359400@163.com>
2020-11-30 15:59:30 +0800

    111

--- src/main/java/com/dragon/力扣/数学问题/OFFER二进制中一的个数.java
+++ src/main/java/com/dragon/力扣/数学问题/OFFER二进制中一的个数.java
@@ -1,12 +1,15 @@
 package com.dragon.力扣.数学问题;
 
 public class OFFER二进制中一的个数 {
+    //
     public int hammingWeight(int n) {
-        int result = 0;
+        int count = 0;
         while(n!=0){
-            result += n&1;
-            n >>>= 1;   //题目要求为无符号数，则使用无符号位移
+            if((n&1)==1){
+                count++;
+            }
+            n >>>=1; //无符号右移
         }
-        return result;
+        return count;
     }
 }
