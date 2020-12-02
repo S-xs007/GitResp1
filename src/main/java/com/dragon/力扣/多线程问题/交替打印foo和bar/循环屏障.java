fe180e56da69c69dfe7b532fbb8f9129a5e236ff
Szx <18524359400@163.com>
2020-12-02 15:06:11 +0800

    jj

---
+++ src/main/java/com/dragon/力扣/多线程问题/交替打印foo和bar/循环屏障.java
@@ -0,0 +1,43 @@
+package com.dragon.力扣.多线程问题.交替打印foo和bar;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class 循环屏障 {
+
+    /**
+     * 通过循环屏障
+     */
+    class FooBar1 {
+        private int n;
+
+        public FooBar1(int n) {
+            this.n = n;
+        }
+
+        CyclicBarrier cb = new CyclicBarrier(2);
+        volatile boolean fin = true;
+
+        public void foo(Runnable printFoo) throws InterruptedException {
+            for (int i = 0; i < n; i++) {
+                while(!fin);
+                printFoo.run();
+                fin = false;
+                try {
+                    cb.await();
+                } catch (BrokenBarrierException e) {
+                }
+            }
+        }
+        public void bar(Runnable printBar) throws InterruptedException {
+            for (int i = 0; i < n; i++) {
+                try {
+                    cb.await();
+                } catch (BrokenBarrierException e) {
+                }
+                printBar.run();
+                fin = true;
+            }
+        }
+    }
+}
